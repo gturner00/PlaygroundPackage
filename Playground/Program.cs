7e50f94f1e11f952099f8ccac2f7d3f1d28b2453
Gary Turner <Gary Turner>
2017-02-11 20:28:41 -0800

    updates

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StatsdClient; 
 
 namespace Playground
 {
@@ -10,6 +11,27 @@ namespace Playground
     {
         static void Main(string[] args)
         {
+            StatsdClient.StatsdConfig config = new StatsdClient.StatsdConfig()
+            {
+                Prefix = "Playground",
+                StatsdPort = 8125,
+                StatsdServerName = "127.0.0.1"
+            };
+            StatsdClient.DogStatsd.Configure(config);
+
+            //StackifyLib.Logger.GlobalApiKey = "3Gz8Sh1Do9Yl3Uo0Mx6Zq4Wa2Oo9Qa3Wl6Ud0Ly";
+            for (int i = 0; i < 1000; i++)
+            {
+                DogStatsd.Counter<int>("Run Count", 1);
+                using (DogStatsd.StartTimer("Run Time"))
+                {
+                    Random random = new Random();
+
+                    System.Threading.Thread.Sleep(random.Next(0, 60) * 100);
+                }
+                Console.WriteLine(string.Format("Metric Count {0}", i));
+
+            }
         }
     }
 }
